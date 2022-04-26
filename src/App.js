1a25d36e92a8894f91a9542da9af6c551cb62efb
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2022-04-26 15:00:54 +1000

    code before api integration

--- src/App.js
+++ src/App.js
@@ -1,57 +1,56 @@
-
-import './App.css';
-import React, {useState} from 'react'
-import { BadList } from './components/BadList';
-import { Form } from './components/Form';
-import { TaskList } from './components/TaskList';
-import { Title } from './components/Title';
-import { TotalHours } from './components/TotalHours';
+import "./App.css";
+import React, { useState } from "react";
+import { BadList } from "./components/BadList";
+import { Form } from "./components/Form";
+import { TaskList } from "./components/TaskList";
+import { Title } from "./components/Title";
+import { TotalHours } from "./components/TotalHours";
 
 function App() {
   const [taskList, setTaskList] = useState([]);
   const [badList, setBadList] = useState([]);
 
-  const addNewTask = task => {
+  const addNewTask = (task) => {
     setTaskList([...taskList, task]);
-  }
+  };
 
-  // delete Task List 
-  const handleOnDeleteTaskList = i => {
-      console.log(i);
-      
-      if(window.confirm("Are you sure you want to delete this task?")) {
-      const newArg = taskList.filter((item, index)=> index !== i) 
+  // delete Task List
+  const handleOnDeleteTaskList = (i) => {
+    console.log(i);
+
+    if (window.confirm("Are you sure you want to delete this task?")) {
+      const newArg = taskList.filter((item, index) => index !== i);
       setTaskList(newArg);
-      }
-  }
+    }
+  };
 
   // take item from taskList and put in the Badlist
-  const markAsNotToDo = i  => {
+  const markAsNotToDo = (i) => {
     const selectedItem = taskList[i];
     setBadList([...badList, selectedItem]);
-    const newArg = taskList.filter((item, index)=> index !== i) 
+    const newArg = taskList.filter((item, index) => index !== i);
     setTaskList(newArg);
-  }
+  };
 
   // Delete Bad List
-  const handleOnDeleteBadList = i => {
+  const handleOnDeleteBadList = (i) => {
     console.log(i);
-    
-    if(window.confirm("Are you sure you want to delete this bad list?")) {
-    const newArg = badList.filter((item, index)=> index !== i) 
-    setBadList(newArg);
+
+    if (window.confirm("Are you sure you want to delete this bad list?")) {
+      const newArg = badList.filter((item, index) => index !== i);
+      setBadList(newArg);
     }
-  }
+  };
 
   //take item from badlist and put in the task list
-  const markAsToDo = i  => {
+  const markAsToDo = (i) => {
     const selectedItem = badList[i];
     setTaskList([...taskList, selectedItem]);
-    const newArg = badList.filter((item, index)=> index !== i) 
+    const newArg = badList.filter((item, index) => index !== i);
     setBadList(newArg);
-  }
+  };
 
-  const savedHours = badList.reduce((subttl, item) => subttl + item.hr , 0);
+  const savedHours = badList.reduce((subttl, item) => subttl + item.hr, 0);
   const ttlTaskHours = taskList.reduce((subttl, item) => subttl + item.hr, 0);
 
   const ttlHours = savedHours + ttlTaskHours;
@@ -63,26 +62,31 @@ function App() {
     <div className="">
       <div className="wrapper">
         <div className="container">
-            {/* <!-- TOP TITLE --> */}
-            <Title />
+          {/* <!-- TOP TITLE --> */}
+          <Title />
 
-            {/* <!-- Form Box --> */}
-                
-            <Form addNewTask={addNewTask} ttlHours={ttlHours}/>
-                
-            {/* <!-- list area --> */}
-            <div className="row">
+          {/* <!-- Form Box --> */}
 
-                <TaskList taskList={taskList} handleOnDeleteTaskList={handleOnDeleteTaskList} markAsNotToDo={markAsNotToDo} />
-                
-                <BadList badList={badList} handleOnDeleteBadList={handleOnDeleteBadList} markAsToDo={markAsToDo} savedHours={savedHours}/>
-               
-                
-            </div>
-            
-            {/* Total HOurs */}
-            <TotalHours total={ttlHours}/>
+          <Form addNewTask={addNewTask} ttlHours={ttlHours} />
 
+          {/* <!-- list area --> */}
+          <div className="row">
+            <TaskList
+              taskList={taskList}
+              handleOnDeleteTaskList={handleOnDeleteTaskList}
+              markAsNotToDo={markAsNotToDo}
+            />
+
+            <BadList
+              badList={badList}
+              handleOnDeleteBadList={handleOnDeleteBadList}
+              markAsToDo={markAsToDo}
+              savedHours={savedHours}
+            />
+          </div>
+
+          {/* Total HOurs */}
+          <TotalHours total={ttlHours} />
         </div>
       </div>
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,79 +1,86 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-export const Form = ({addNewTask, ttlHours}) => {
+export const Form = ({ addNewTask, ttlHours }) => {
+  const weeklyHours = 24 * 7;
+  const initialState = {
+    task: "",
+    hr: "",
+  };
 
-    const weeklyHours = 24 * 7;
-    const initialState = {
-        task : "",
-        hr: ""
-    }
-    
-    const [newTask, setNewTask] = useState(initialState);
+  const [newTask, setNewTask] = useState(initialState);
 
+  const handleOnChange = (e) => {
+    const { value, name } = e.target;
+    setNewTask({
+      ...newTask,
+      [name]: name === "hr" ? +value : value,
+    });
+    console.log(newTask);
+  };
 
-    const handleOnChange = e => {
-        const {value, name} = e.target;
-        setNewTask({
-            ...newTask,
-            [name] : name === "hr" ? +value : value,
-        });
-        
-    };
-
-    const handleOnSubmit = e => {
-
-      e.preventDefault();
-      if(newTask.hr <1){
-        return alert("Please enter a valid number of hour")
-      }
-
-        if(ttlHours + newTask.hr > weeklyHours) {
-          return alert("You Have Reached Maximum Hours");
-        }
-        
-        addNewTask(newTask);
-        setNewTask(initialState);
-        document.getElementById("inlineFormInputGroupUsername").focus();
-
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (newTask.hr < 1) {
+      return alert("Please enter a valid number of hour");
     }
 
+    if (ttlHours + newTask.hr > weeklyHours) {
+      return alert("You Have Reached Maximum Hours");
+    }
+
+    addNewTask(newTask);
+    setNewTask(initialState);
+    document.getElementById("inlineFormInputGroupUsername").focus();
+  };
 
   return (
     <div className="row">
-    <div className="col">
+      <div className="col">
         <div className="form-box py-5">
-            <form 
+          <form
             onSubmit={handleOnSubmit}
-            className="row row-cols-lg-auto g-3 align-items-center d-flex justify-content-center g-3">
-                <div className="col-12">
-                  <label className="visually-hidden">Your Task</label>
-                  <div className="input-group">
-                    
-                    <input name = "task" type="text"
-                     className="form-control" 
-                    value = {newTask.task}
-                    onChange={handleOnChange}
-                    id="inlineFormInputGroupUsername" placeholder="Your Task" required />
-                  </div>
-                </div>
+            className="row row-cols-lg-auto g-3 align-items-center d-flex justify-content-center g-3"
+          >
+            <div className="col-12">
+              <label className="visually-hidden">Your Task</label>
+              <div className="input-group">
+                <input
+                  name="task"
+                  type="text"
+                  className="form-control"
+                  value={newTask.task}
+                  onChange={handleOnChange}
+                  id="inlineFormInputGroupUsername"
+                  placeholder="Your Task"
+                  required
+                />
+              </div>
+            </div>
 
-                <div className="col-12">
-                    <label className="visually-hidden">Hours</label>
-                    <div className="input-group">
-                      
-                      <input name = "hr" type="text" className="form-control"
-                      value = {newTask.hr}
-                      onChange={handleOnChange}
-                      id="inlineFormInputGroupUsername" placeholder="Hours" required />
-                    </div>
-                  </div>
-              
-                <div className="col-12">
-                  <button type="submit" className="btn btn-primary">Submit</button>
-                </div>
-              </form>
+            <div className="col-12">
+              <label className="visually-hidden">Hours</label>
+              <div className="input-group">
+                <input
+                  name="hr"
+                  type="text"
+                  className="form-control"
+                  value={newTask.hr}
+                  onChange={handleOnChange}
+                  id="inlineFormInputGroupUsername"
+                  placeholder="Hours"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="col-12">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
